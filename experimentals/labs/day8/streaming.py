5f6f3b581f0d284b3c94a0db7cc6f83a226c22f7
praveen.s@essilor.co.th <praveen.s@essilor.co.th>
2025-09-18 23:40:14 +0700

    Delete Unuse

--- experimentals/labs/day8/streaming.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-# MAGIC %sql
-# MAGIC SHOW TABLES in catalog_yutthanal.steam
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC SELECT * FROM catalog_yutthanal.steam.transformed_iot_data
